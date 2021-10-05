a8b8b384ce8cb32944909338b08a5575206dd684
Mun-cloud <mun05170@gmail.com>
2021-10-05 19:49:36 +0900

    ffmpeg사용법 1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import {createFFmpeg, fetchFile} from "@ffmpeg/ffmpeg"
 const startBtn = document.getElementById("startBtn")
 const video = document.getElementById("preview")
 
@@ -5,7 +6,15 @@ let stream
 let recorder
 let videoFile
 
-const handleDownload = () => {
+const handleDownload = async () => {
+
+    const ffmpeg = createFFmpeg({log:true})
+    await ffmpeg.load() //자바스크립트가 아닌 다른 소프트웨어를 사용하는 것이므로 await 해줘야 함.
+
+    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile)) // FS파일시스템. 생성, 파일명, 데이터
+
+    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4") // ffmpeg로 run 실행. 명령어 다양함. 가상 공간에 있는 recording.webm 파일을 -i로 인풋 받아 output.mp4로 출력함. 초당 60프레임으로.
+
     const a = document.createElement("a")
     a.href = videoFile
     a.download = "MyRecording.mp4" //a에 download라는 속성을 추가시키면 클릭시 다운을 시켜줌
